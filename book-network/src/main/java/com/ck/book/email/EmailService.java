1e6523d7b1179b149d103a363a9274bd22c0a191
chunkityip <alex0917lfo@hotmail.com>
2024-04-20 16:30:23 -0400

    Finish at 2:46:03

--- book-network/src/main/java/com/ck/book/email/EmailService.java
+++ book-network/src/main/java/com/ck/book/email/EmailService.java
@@ -3,46 +3,64 @@ package com.ck.book.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class EmailService {
-
-    private final JavaMailSender javaMailSender;
+    private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
 
-    public void sendEmail(String to ,
-                          String username ,
-                          EmailTemplateName emailTemplateName ,
-                          String confirmationUrl,
-                          String activationCode,
-                          String subject
+    @Async
+    public void sendEmail(
+            String to,
+            String username,
+            EmailTemplateName emailTemplate,
+            String confirmationUrl,
+            String activationCode,
+            String subject
     ) throws MessagingException {
         String templateName;
-        if (emailTemplateName == null) {
+        if (emailTemplate == null) {
             templateName = "confirm-email";
         } else {
-            templateName = emailTemplateName.name();
+            templateName = emailTemplate.name();
         }
-
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
                 mimeMessage,
-                MimeMessageHelper.MULTIPART_MODE_MIXED,
+                MULTIPART_MODE_MIXED,
                 UTF_8.name()
         );
-        Map<String , Object> properties = new HashMap<>();
-        properties.put("username" , username);
-        properties.put("confirmationUrl" , confirmationUrl);
-        properties.put("activation_code" , activationCode);
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activation_code", activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        helper.setFrom("yck11214@gmail.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String template = templateEngine.process(templateName, context);
+
+        helper.setText(template, true);
+
+        mailSender.send(mimeMessage);
     }
 }

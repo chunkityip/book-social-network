5bd856d02a6ed837210e82fa0613878eced7b813
chunkityip <alex0917lfo@hotmail.com>
2024-08-18 15:06:19 -0400

    implement the login page

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {AuthenticationResponse} from "../../services/models/authentication-response";
+import {TokenService} from "../../token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -10,4 +14,33 @@ export class LoginComponent {
   authRequest: AuthenticationRequest = {email:'', password:''};
   errorMsg: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: TokenService
+  ) {}
+
+  login() {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res:AuthenticationResponse): void => {
+        this.tokenService.token = res.token as string
+        this.router.navigate(['books']);
+      },
+      error: (err): void => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors
+        } else {
+          this.errorMsg.push(err.error.errorMsg)
+        }
+      }
+    });
+  }
+
+  register() {
+    this.router.navigate(['register'])
+  }
 }
